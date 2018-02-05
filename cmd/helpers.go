963735a131ac5827285241ffe29a0fa65fc7033f
Amit Saha <amitsaha.in@gmail.com>
2018-02-05 17:34:32 +1100

    launch-more-like: Support for editing user data before launching the instance

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 )
@@ -17,4 +20,44 @@ func createSession() *session.Session {
 		Profile:           profile,
 	}))
 	return sess
+}
+
+func modifyUserData(userData string) (*string, error) {
+	vi := "vim"
+	tmpDir := os.TempDir()
+	tmpFile, tmpFileErr := ioutil.TempFile(tmpDir, "tempFilePrefix")
+	if tmpFileErr != nil {
+		fmt.Printf("Error %s while creating tempFile", tmpFileErr)
+	}
+
+	err := ioutil.WriteFile(tmpFile.Name(), []byte(userData), 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(tmpFile.Name())
+
+	path, err := exec.LookPath(vi)
+	if err != nil {
+		fmt.Printf("Error %s while looking up for %s!!", path, vi)
+		return nil, err
+	}
+
+	cmd := exec.Command(path, tmpFile.Name())
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("Waiting for command to finish.\n")
+	err = cmd.Wait()
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the contents back
+	editedFileContents, err := ioutil.ReadFile(tmpFile.Name())
+	editedFileContentsStr := string(editedFileContents)
+	return &editedFileContentsStr, err
 }
