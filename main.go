f510f355c19f0a4f3224cef9ffd72400c5498863
Amit Saha <amitsaha.in@gmail.com>
2019-05-24 16:54:20 +1000

    native bash completion

--- main.go
+++ main.go
@@ -14,8 +14,60 @@
 
 package main
 
+import "os"
+import "fmt"
 import "github.com/amitsaha/yawsi/cmd"
+import "strings"
 
 func main() {
-	cmd.Execute()
+	if len(os.Getenv("COMP_LINE")) != 0 {
+		if len(os.Getenv("COMP_DEBUG")) != 0 {
+			fmt.Printf("%#v\n", os.Args[1:])
+		}
+		// whose commands is being completed
+		if os.Args[1] == "yawsi" {
+			// no sub-commands yet: yawsi <TAB>
+			if os.Args[2] == "" && os.Args[3] == "yawsi" {
+				for _, cmd := range cmd.RootCmd.Commands() {
+					fmt.Printf("%s\n", cmd.Use)
+				}
+			}
+
+			// yawsi <valid-subcommand> <TAB>
+			if len(os.Args[2]) == 0 && len(os.Args[3]) != 0 && os.Args[3] != "yawsi" {
+				for _, cmd := range cmd.RootCmd.Commands() {
+					if cmd.Use == os.Args[3] {
+						for _, cmd := range cmd.Commands() {
+							fmt.Printf("%s\n", cmd.Use)
+						}
+						for _, arg := range cmd.ValidArgs {
+							fmt.Printf("%s\n", arg)
+						}
+						flags := cmd.NonInheritedFlags()
+                                                fmt.Printf("Flags: %#v\n", flags)
+
+					}
+				}
+			}
+			// yawsi <valid-subcommand> <valid-subcommand/arg><TAB>
+			if len(os.Args[2]) != 0 && len(os.Args[3]) != 0 && os.Args[3] != "yawsi" {
+				for _, cmd := range cmd.RootCmd.Commands() {
+					if cmd.Use == os.Args[3] {
+						for _, cmd := range cmd.Commands() {
+							if strings.HasPrefix(cmd.Use, os.Args[2]) || os.Args[2] == cmd.Use {
+								fmt.Printf("%s\n", cmd.Use)
+							}
+						}
+						for _, arg := range cmd.ValidArgs {
+							fmt.Printf("%s\n", arg)
+						}
+
+					}
+				}
+			}
+
+		}
+	} else {
+		cmd.Execute()
+	}
 }
